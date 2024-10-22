a9a7a8c310c7057bec8169f1db35de19f29145bc
IshkhanB <b2068666@gmail.com>
2024-10-22 20:44:25 +0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  },
+
+  app:{
+    baseURL: '/nuxt-project/',
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
+
+  nitro:{
+    plugins: ['plugins/extend-html.ts']
+  },
+
+  modules: ['@pinia/nuxt']
+})
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+  <img src="/favicon.ico" alt="favicon">
+
+  <h1>Hello from index Page</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>

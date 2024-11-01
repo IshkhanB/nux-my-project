55568f1334914ca2bf49ab883f8b6423cc1c3ea6
IshkhanB <b2068666@gmail.com>
2024-11-01 18:09:44 +0300

    111

--- app.vue
+++ app.vue
@@ -1,4 +1,5 @@
 <template>
+  <HeaderPage></HeaderPage>
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
---
+++ components/HeaderPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div >
+    <h1 style="text-align: center;">Блог Васи</h1>
+    <!-- <section> -->
+      <header class="header">
+        <nav>
+          <a href="?t=-1d">Вчера</a>;
+          <a href="?t=-7d">Прошлая неделя</a>;
+          <a href="?t=-1m">Прошлый месяц</a>
+        </nav>
+        <h2>Я прочитал всю спецификацию EcmaScript!</h2>
+      </header>
+    </div>
+</template>
+<style>
+.header{
+  margin: 0 auto;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>

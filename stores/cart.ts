dfec268d23fd2c6487e66594f035b21391f79786
IshkhanB <B2068666@gmail.com>
2025-07-08 21:18:04 +0300

    start

--- stores/cart.ts
+++ stores/cart.ts
@@ -1,6 +1,5 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
-// import { Prisma } from '@prisma/client'
 
 export const useCart = defineStore('cart', () => {
   const arr = ref([] as any)
